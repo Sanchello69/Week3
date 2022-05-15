14b29cfdcf37168aa26575d804c5f9ce988691b3
Alexandr <sanchello2001@yandex.ru>
2022-05-15 17:31:59 +0300

    add Facebook fragments

---
+++ app/src/main/java/com/vas/week3/FacebookFragment1.kt
@@ -0,0 +1,23 @@
+package com.vas.week3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+
+class FacebookFragment1 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        activity?.window?.statusBarColor = ResourcesCompat.getColor(resources,
+            R.color.facebook, null)
+
+        return inflater.inflate(R.layout.fragment_facebook1, container, false)
+    }
+
+}
